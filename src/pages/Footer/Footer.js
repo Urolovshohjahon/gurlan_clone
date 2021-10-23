3954c0b536481adb0c90904643e5630d6c064ae0
Urolovshohjahon <urolovshohjahon@gmail.com>
2021-10-23 23:36:01 +0500

    okey

---
+++ src/pages/Footer/Footer.js
@@ -0,0 +1,164 @@
+import React, { useState, useEffect } from 'react';
+import { AiFillYoutube } from 'react-icons/ai';
+import { FaFacebookF, FaInstagram, FaTelegram } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import './Footer.css';
+import { NavLink } from 'react-router-dom';
+import { getNews } from '../../host/Config';
+import { url, user } from '../../host/Host';
+import axios from 'axios';
+import Global from '../../host/Global';
+import { Clock } from '../Clock';
+
+function Footer() {
+  const [loader, setLoader] = useState(true);
+  const [news, setNews] = useState([]);
+  const [id, setId] = useState(0);
+  const [school, setSchool] = useState({});
+  const [clock, setClock] = useState('00 : 00 : 00');
+  const [state, setState] = useState({
+    loader: true,
+    news: [],
+    id: 0,
+    school: null,
+    clock: '00 : 00 : 00',
+  });
+
+  const getSchool = () => {
+    axios
+      .get(`${url}/school-by-admin/${Global.user}`)
+      .then((res) => {
+        setSchool(res.data);
+        setTimeout(() => {
+          setLoader(false);
+        }, 2000);
+      })
+      .catch((err) => {
+        console.log('Maktabda xatolik bor');
+      });
+  };
+  /* const getNew = () => {
+    getNews()
+      .then((res) => {
+        if (window.location.href.indexOf('id=') === -1) {
+          // var a = window.location.href.split("/");
+          var v = user;
+
+          setState({
+            news: res.data,
+            id: v,
+          });
+        } else {
+          setState({
+            news: res.data,
+            id: window.location.href.slice(
+              window.location.href.indexOf('=') + 1
+            ),
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("Xatolar daxxuya");
+      });
+  }; */
+
+  useEffect(() => {
+    /*  getNew(); */
+    getSchool();
+    window.addEventListener('load', () => {
+      setLoader(false);
+    });
+
+    /*  setInterval(() => {
+      setState({ clock: Clock() });
+    }, 1000); */
+  }, []);
+  return (
+    <footer>
+      <div className='container'>
+        <div className='row d-flex justify-content-center'>
+          <div className='col-lg-6 d-flex justify-content-between'>
+            <div className='contact'>
+              <a href='https://www.youtube.com/channel/UCTU9AVjpeZQLSSh3rlwTpDw'>
+                <AiFillYoutube className='contact_icons youtube' />
+              </a>
+              <a href='https://www.facebook.com/5-sonli-maktab-283432626789189/?ref=pages_you_manage'>
+                <FaFacebookF className='contact_icons facebook' />
+              </a>
+              <a
+                href={`mailto: ${
+                  school !== null ? school.email : 'ittower01@gmail.com'
+                }`}
+              >
+                <HiOutlineMail className='contact_icons hiemail' />
+              </a>
+              <a href='https://t.me/Xorazm_5_maktab'>
+                {' '}
+                <FaTelegram className='contact_icons telegram' />
+              </a>
+              <a href='https://www.instagram.com/5ummumtalimmaktabi/'>
+                <FaInstagram className='contact_icons instagram' />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-lg-4'>
+            <ul>
+              <h3>Bizning maktab</h3>
+              <li>
+                <NavLink to='/'>Bosh sahifa</NavLink>
+              </li>
+              <li>
+                <NavLink to='/hayot'>Maktab hayoti</NavLink>
+              </li>
+              <li>
+                <NavLink to='/qabul'>Qabul</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className='col-lg-4'>
+            <ul>
+              <h3>Maktab hayoti</h3>
+
+              <li>
+                <NavLink to='/news'>Yangiliklar</NavLink>
+              </li>
+              <li>
+                <NavLink to='rahbariyat'>Maktab ma'muriyati</NavLink>
+              </li>
+              <li>
+                <NavLink to='/alochilar'>Maktab a'lochilari</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className='col-lg-4'>
+            <ul>
+              <h3>Maktab ma'lumoti</h3>
+
+              <li>
+                <NavLink to='/'>Xorazm viloyati Gurlan tumani</NavLink>
+              </li>
+              <li>
+                <NavLink to='/'>
+                  <a
+                    href={`mailto: ${
+                      school !== null ? school.email : 'ittower01@gmail.com'
+                    }`}
+                  >
+                    xorazm5maktab@gmail.comi
+                  </a>
+                </NavLink>
+              </li>
+              <li>
+                <a target='_blank' href={`tel: ${school.phone}`}>+998930820372</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/News/News.js
@@ -0,0 +1,156 @@
+import React, { useEffect, useState } from 'react';
+import HEADER_NAV from '../Home/Header_nav';
+import Footer from '../Footer/Footer';
+import './News.css';
+import { AiFillPhone } from 'react-icons/ai';
+import { Carousel } from 'react-bootstrap';
+import rasm2 from '../../img/news.jpg';
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import { connect, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import axios from 'axios';
+import { url, user } from '../../host/Host';
+import { getNews } from '../../host/Config';
+import school1 from '../../img/school1.jpg';
+import Spinner from '../Spinner/Spinner';
+
+/* const news = [
+  {
+    title: 'YAngilik1',
+    main: 'Yangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopzVVVYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kop',
+    img: 'https://agitated-lamport-037c23.netlify.app/static/media/3s-Photo-3.c4a2f2b9.jpg',
+  },
+  {
+    title: 'YAngilik2',
+    main: 'Yangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopzVVVYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kop',
+    img: 'https://agitated-lamport-037c23.netlify.app/static/media/1.feb51666.jpg',
+  },
+  {
+    title: 'YAngilik3',
+    main: 'Yangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopzVVVYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kopYangiliklar kop',
+    img: 'https://agitated-lamport-037c23.netlify.app/static/media/love.042dfb21.jpg',
+  },
+]; */
+
+function News(props) {
+  const [loader, setLoader] = useState(true);
+  const [news, setNews] = useState([]);
+  const [id, setId] = useState(0);
+  const [school, setSchool] = useState(null);
+
+  const getSchool = () => {
+    var v = user;
+    axios.get(`${url}/school-by-admin/${v}`).then((res) => {
+      setSchool(res.data);
+      setLoader(false);
+    });
+  };
+
+  const getNew = () => {
+    getNews()
+      .then((res) => {
+        if (window.location.href.indexOf('id=') === -1) {
+          setNews(res.data);
+        } else {
+          setNews(res.data);
+          setId(
+            window.location.href.slice(window.location.href.indexOf('=') + 1)
+          );
+        }
+      })
+      .catch((err) => {
+        console.log('blaaaaa...');
+      });
+  };
+
+  useEffect(() => {
+    getNew();
+    getSchool();
+  });
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const gotonews = (key) => {
+    console.log('1');
+    dispatch({
+      type: 'ENTER_NEWS_PAGE',
+      title: news[key].title,
+      main: news[key].text,
+      img: news[key].image,
+      date:news[key].published_time.substring(0, 10),
+      phone:school.phone
+    });
+    console.log('2');
+    history.push('/new');
+    console.log('3');
+  };
+
+  return (
+    <div>
+      {loader ? (
+        <Spinner />
+      ) : (
+        <div>
+          <Carousel fade>
+            {news.map((val, key) => (
+              <Carousel.Item>
+                <img
+                  className='d-block w-100'
+                  src={val.image}
+                  alt='First slide'
+                />
+                <Carousel.Caption>
+                  <h3>{val.title}</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+          <a
+            href={`tel: ${
+              school !== null ? school.phone : '+998 93 082 03 72'
+            }`}
+          ></a>
+          <div className='container'>
+            <div className='row '>
+              {news.map((val, key) => (
+                <div className='col-lg-4'>
+                  {/* <NavLink  to='/new' > */}
+                  <div className='card news_card' onClick={() => gotonews(key)}>
+                    <img src={val.image} class='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <p className=' d-flex align-items-center'>
+                        <FaRegCalendarAlt className='date_icon' color='blue' />{' '}
+                        {val.published_time.substring(0, 10)}
+                      </p>
+                      <h5 className='card-title'>{val.title}</h5>
+                    </div>
+                  </div>
+                  {/* </NavLink> */}
+                </div>
+              ))}
+            </div>
+          </div>
+          <Footer />
+          <HEADER_NAV />
+        </div>
+      )}
+    </div>
+  );
+}
+
+/* const mapDispatchToProps = (dispatch) => {
+  return {
+    setNews: () =>
+      dispatch({
+        type: 'ENTER_NEWS_PAGE',
+        title: news[0].title,
+        main: news[0].main,
+        img: news[0].img,
+      }),
+  };
+}; */
+
+export default connect(null, null)(News);
